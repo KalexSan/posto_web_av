0cd3cf08c36e1bf2f94213b7dec38a244276479a
Alex Sander Leite de Almeida <66784006+KalexSan@users.noreply.github.com>
2024-09-03 21:47:38 -0300

    pequenas mudanças

--- src/main/java/br/edu/utfpr/td/tsi/DAO/MysqlConsultaDAO.java
+++ src/main/java/br/edu/utfpr/td/tsi/DAO/MysqlConsultaDAO.java
@@ -8,9 +8,11 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import br.edu.utfpr.td.tsi.MODELO.Consulta;
 
+@Component
 public class MysqlConsultaDAO implements ConsultaDAO {
 
     @Autowired
